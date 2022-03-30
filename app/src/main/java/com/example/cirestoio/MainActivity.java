fc7f37dd6e34cd214eaa071fcd6f003215adc32b
DavideFiloni <filonidavide@gmail.com>
2022-03-30 10:23:17 +0200

    30/03/2022 : Permessi microfono

--- app/src/main/java/com/example/cirestoio/MainActivity.java
+++ app/src/main/java/com/example/cirestoio/MainActivity.java
@@ -75,7 +75,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             //i permessi per l'uso della camera non sono stati concessi, quindi bisogna richiederli
             ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST);
         }
-        // a questo punto i permessi per la camera sono già stati concessi, per cui è possibile procedere ad operare
+        // a questo punto i permessi per la camera sono già stati concessi
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            //i permessi per l'uso del microfono non sono stati concessi, quindi bisogna richiederli
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+        }
+        // a questo punto anche i permessi per il microfono sono già stati concessi, per cui è possibile procedere ad operare
         return true;
     }
 
