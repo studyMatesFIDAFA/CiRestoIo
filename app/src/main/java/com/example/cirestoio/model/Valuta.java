8bac1d87fed86f7bd9357c84ce5c9c10fa783604
fabioscaglia <fabioscagliarini99@gmail.com>
2022-04-06 09:53:43 +0200

    6/04/2022 Classe Valuta

---
+++ app/src/main/java/com/example/cirestoio/model/Valuta.java
@@ -0,0 +1,52 @@
+package com.example.cirestoio.model;
+
+import java.util.Objects;
+
+public class Valuta {
+    private String nome;
+    private String nazione;
+    private double eurRate;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNazione() {
+        return nazione;
+    }
+
+    public void setNazione(String nazione) {
+        this.nazione = nazione;
+    }
+
+    public double getEurRate() {
+        return eurRate;
+    }
+
+    public void setEurRate(double eurRate) {
+        this.eurRate = eurRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Valuta valuta = (Valuta) o;
+        return Double.compare(valuta.eurRate, eurRate) == 0 && Objects.equals(nome, valuta.nome) && Objects.equals(nazione, valuta.nazione);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, nazione, eurRate);
+    }
+
+    public Valuta(String nome, String nazione, double eurRate) {
+        this.nome = nome;
+        this.nazione = nazione;
+        this.eurRate = eurRate;
+    }
+}
